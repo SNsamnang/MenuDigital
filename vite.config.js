f201626855206c8e51e351f8e2f9ed10988ed68b
SNsamnang <naosamnang07@gmail.com>
2025-05-07 15:50:21 +0700

    rebuild

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: process.env.VIT_BASE_PATH || "/MenuDigital",
+  build: {
+    chunkSizeWarningLimit: 1000, 
+  },
 });
