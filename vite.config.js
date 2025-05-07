b3e7207cd9d3a64efa81c95b7eac2ec863448329
SNsamnang <naosamnang07@gmail.com>
2025-05-07 17:16:16 +0700

    verify

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VIT_BASE_PATH || "/MenuDigital",
-  build: {
-    chunkSizeWarningLimit: 1000, 
-  },
+  base: process.env.VITE_BASE_PATH || "/MenuDigital",
+  
 });
