95ca20375325a294f73a86b07c0d3893f1dbc61d
SNsamnang <144607379+SNsamnang@users.noreply.github.com>
2025-05-07 16:41:25 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: process.env.VIT_BASE_PATH || "/MenuDigital",
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'], // Split vendor libraries
-        },
-      },
-    },
-  },
+  
 });
